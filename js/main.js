6b2b234961aa17da6e4c1dd9b90a7da3d5e4bc6d
Ramune1101 <makovv12@naver.com>
2025-01-23 18:44:44 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
- 
+ document.addEventListener('DOMContentLoaded', function() {
+    // 현재 시간을 표시
+    const deployTime = new Date().toLocaleString();
+    document.getElementById('deployTime').textContent = deployTime;
+}); 
